cb6ede56e0db8958a84e40b39ddc99a07dd7d3e9
Zaibten Info <113015136+Zaibten@users.noreply.github.com>
2025-05-19 01:04:33 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 import Register from "./components/Register";
@@ -28,7 +29,6 @@ const Home = () => {
         <Team />
         <Contact />
         <Footer />
-        <Register/>
       </div>
       <ButtonGradient />
       <ScrollToTopButton />
