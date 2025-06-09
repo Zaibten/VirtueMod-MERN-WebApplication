0b1aca90ec093f39bc10339f42c8b0a755a1e7c4
Zaibten Info <113015136+Zaibten@users.noreply.github.com>
2025-06-09 21:33:27 +0500

    Update ThreeDModel.jsx

--- client/src/components/ThreeDModel.jsx
+++ client/src/components/ThreeDModel.jsx
@@ -63,7 +63,7 @@ const ThreeDModel = () => {
       >
         <iframe
           title="3D Model Viewer"
-          src="https://26023128f81f1093ce.gradio.live/"
+          src="https://7e4da48e3284bf4c99.gradio.live/"
           // src={iframeSrc}
           allow="autoplay; fullscreen; vr"
           allowFullScreen
